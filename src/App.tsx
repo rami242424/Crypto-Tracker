82058f91616b22608b5dd333088b043411ba2ef8
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-29 20:20:00 +0900

    3.6 Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
-import React, { useState } from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor}
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor}
+`;
 function App(){
-  // value: 현재 저장된 값
-  const [value, setValue] = useState("");
-  const onChange = (event:React.FormEvent<HTMLInputElement>) => {
-    // console.log(event.currentTarget.value);
-    // 여기서 value는 input에 방금 입력된 값!
-    const {currentTarget : {value}} = event;
-    // input에 입력한 값을 상태로 저장
-    setValue(value); // === setValue(event.currentTarget.value)
-  };
-  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value); // === 최신 상태값(useState의 value)
-    setValue("");
-  }
+  
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input 
-          type="text" 
-          placeholder="username"
-          onChange={onChange}
-          value={value}
-        />
-        <button>Log in</button>
-      </form>
+      <Container>
+        <H1>Protected</H1>
+      </Container>
     </div>
   );
 }
---
+++ src/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+    export interface DefalutTheme {
+        textColor: string;
+        bgColor: string;
+        btnColor: string;
+    }
+}

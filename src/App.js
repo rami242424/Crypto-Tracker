f02c6bc0e7750bf13779970def50b347ac9d5c5f
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-29 11:43:56 +0900

    2.3 'as'

--- src/App.js
+++ src/App.js
@@ -4,21 +4,19 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border:0;
+  border-radius:15px;
 `;
-const Circle = styled(Box)`
-  border-radius : 50px;
-`;
+
 
 function App(){
   return (
     <Father>
-      <Box bgColor="yellow"/>
-      <Box bgColor="teal"/>
-      <Circle bgColor="blue"/>
+      <Btn>Log In</Btn>
+      <Btn as="a" href="/">Log Out</Btn>
     </Father>
   );
 }

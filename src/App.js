7291173f94ddac99736181968f288100379abc89
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-29 11:57:07 +0900

    2.4 Animations

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const rotateAnimation = keyframes`
+  0%{
+    transform:rotate(0deg);
+    border-radius: 0px;
+  }
+  25%{
+    transform:rotate(180deg);
+    border-radius: 50px;
+  }
+  50%{
+    transform:rotate(360deg);
+    border-radius: 100px;
+  }
+  100%{
+    transform:rotate(0deg);
+    border-radius: 0px;
+  }
+`;
+
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({required : true})`
-  background-color: blueviolet;
+const Box = styled.div`
+  height: 100px;
+  width: 100px;
+  background-color: tomato;
+  animation: ${rotateAnimation} 1s linear infinite;
 `;
+
 
 function App(){
   return (
-    <Father>
-      <Input/>
-      <Input/>
-      <Input/>
-      <Input/>
-      <Input/>
-    </Father>
+    <Wrapper>
+      <Box/>
+    </Wrapper>
   );
 }
 

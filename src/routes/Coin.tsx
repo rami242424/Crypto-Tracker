5fae57c0307b015e338ea7a120af085f49f3552a
Rami <138556019+rami242424@users.noreply.github.com>
2025-05-01 10:24:31 +0900

    #5.10 React Query part Two

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -10,6 +10,8 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Chart from "./Chart";
 import Price from "./Price";
+import { useQuery } from "react-query";
+import { fetchCoinInfo, fetchCoinTickers } from "./api";
 
 const Title = styled.h1`
   font-size: 48px;
@@ -85,7 +87,7 @@ interface RouteParams {
 interface RouteState {
   name: string;
 }
-interface InfoData {
+interface IInfoData {
   id: string;
   name: string;
   symbol: string;
@@ -105,7 +107,7 @@ interface InfoData {
   first_data_at: string;
   last_data_at: string;
 }
-interface PriceData {
+interface IPriceData {
   id: string;
   name: string;
   symbol: string;
@@ -140,13 +142,16 @@ interface PriceData {
 }
 
 function Coin() {
-  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
-  const [info, setInfo] = useState<InfoData>();
-  const [priceInfo, setPriceInfo] = useState<PriceData>();
   const priceMatch = useRouteMatch("/:coinId/price");
   const chartMatch = useRouteMatch("/:coinId/chart");
+  const { isLoading: infoLoading, data: infoData } = useQuery<IInfoData>(["info", coinId], () => fetchCoinInfo(coinId));
+  const { isLoading: tickersLoading, data: tickersData} = useQuery<IPriceData>(["tickers", coinId], () => fetchCoinTickers(coinId));
+  
+  /* const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
   useEffect(() => {
     (async () => {
       const infoData = await (
@@ -159,41 +164,44 @@ function Coin() {
       setPriceInfo(priceData);
       setLoading(false);
     })();
-  }, [coinId]);
+  }, [coinId]); */
+
+  const loading = infoLoading || tickersLoading;
+
   return (
     <Container>
       <Header>
         <Title>
-          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+          {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
         </Title>
       </Header>
-      {loading ? (
+      {tickersLoading ? (
         <Loader>Loading...</Loader>
       ) : (
         <>
           <Overview>
             <OverviewItem>
               <span>Rank:</span>
-              <span>{info?.rank}</span>
+              <span>{infoData?.rank}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Symbol:</span>
-              <span>${info?.symbol}</span>
+              <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Open Source:</span>
-              <span>{info?.open_source ? "Yes" : "No"}</span>
+              <span>{infoData?.open_source ? "Yes" : "No"}</span>
             </OverviewItem>
           </Overview>
-          <Description>{info?.description}</Description>
+          <Description>{infoData?.description}</Description>
           <Overview>
             <OverviewItem>
               <span>Total Suply:</span>
-              <span>{priceInfo?.total_supply}</span>
+              <span>{tickersData?.total_supply}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Max Supply:</span>
-              <span>{priceInfo?.max_supply}</span>
+              <span>{tickersData?.max_supply}</span>
             </OverviewItem>
           </Overview>
 
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -6,7 +6,25 @@
 // }
 
 // 방법2 : promise사용
+
+const BASE_URL = `https://api.coinpaprika.com/v1`;
+
 export function fetchCoins(){
-    return fetch("https://api.coinpaprika.com/v1/coins").then((response) => response.json()
+    return fetch(`${BASE_URL}/coins`).then((response) => response.json()
     );
 }
+
+export function fetchCoinInfo(coinId : string){
+    return fetch(`${BASE_URL}/coins/${coinId}`).then((response) => response.json());
+}
+
+export function fetchCoinTickers(coinId : string){
+    return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) => response.json());
+}
+
+
+
+
+// https://api.coinpaprika.com/v1/coins/${coinId}
+// https://api.coinpaprika.com/v1/tickers/${coinId}
+
